0d93aa3d6e3c054d917af2328e87349a66dfef8a
SY97P <>
2022-05-29 06:48:20 +0000

    단어수학 문제 풀이

--- 백준_그리디/단어수학.py
+++ 백준_그리디/단어수학.py
@@ -8,9 +8,9 @@ def solution(file) :
 		for word in words : 
 			for i in range(len(word)-1, -1, -1) : 
 				if word[i] in dic : 
-					dic[word[i]] += pow(10, i)
+					dic[word[i]] += pow(10, (len(word)-i-1))
 				else : 
-					dic[word[i]] = pow(10, i)
+					dic[word[i]] = pow(10, (len(word)-i-1))
 		print(dic)
 		lst = []
 		for key in dic.keys() : 
@@ -28,6 +28,31 @@ def solution(file) :
 		for val in dic.values() : 
 			result += val
 		print(result, answer)
+
+# 백준 제출용
+n = int(input())
+words = [input().strip("\n") for _ in range(n)]
+dic = dict()
+
+for word in words : 
+	for i in range(len(word)-1, -1, -1) :
+		if word[i] in dic : 
+			dic[word[i]] += pow(10, (len(word)-i-1))
+		else : 
+			dic[word[i]] = pow(10, (len(word)-i-1))
+lst = []
+for key in dic.keys() : 
+	lst.append([key, dic.get(key)])
+lst.sort(key = lambda x: x[1], reverse = True)
+
+value = 9
+for i, l in enumerate(lst) : 
+	dic[lst[i][0]] *= value
+	value -= 1
+result = 0
+for val in dic.values() : 
+	result += val
+print(result)
 
 def main() : 
 	file = open("./백준_그리디/단어수학tc.txt", "r")
