5a854f152e3deb27774c8e8381e7df8082dbe741
SY97P <>
2022-06-16 06:50:03 +0000

    수리공 항승 문제 풀이

--- 백준_그리디/수리공항승.py
+++ 백준_그리디/수리공항승.py
@@ -8,19 +8,48 @@ def solution(file) :
 		print(n, l, answer)
 		print(arr)
 
-		count = 0 
-
 		arr.sort()
+		
+		count = 0
+		length = arr[0] - 0.5 + l
 
-		while arr : 
+		while arr :
 			curr = arr.pop(0)
-			length = curr - 0.5 + l
-			count += 1
-			print(curr, length)
-			if len(arr) != 0 and l >= arr[0] + 0.5 : 
-				arr.pop(0)
+			# 현재 지점이 테이프 길이 안에 겹치면
+			# count가 0일 때는 증가
+			# 아닐 때는 증가 x
+			# length 변화도 안하고
+			if curr + 0.5 <= length :
+				if count == 0 :
+					count += 1
+			else :
+				# 현재 지점이 테이프 길이 밖에 있으면
+				# count를 증가해주고
+				# length도 현재 기준으로 수정해주고
+				count += 1
+				length = curr - 0.5 + l
+			# print("curr, length, count : ", curr, length, count)
 
 		print(count)
+		print()
+
+
+# 백준 제출용
+n, l = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+count, length = 0, arr[0] - 0.5 + l
+
+while arr :
+	curr = arr.pop(0)
+	if curr + 0.5 <= length :
+		if count == 0 :
+			count += 1
+	else :
+		count += 1
+		length = curr - 0.5 + l
+print(count)
 
 def main() : 
 	file = open("./백준_그리디/수리공항승tc.txt", "r")
