2e4a066863544f8c30795e50733e0d4083ccd908
SY97P <>
2022-06-27 14:34:00 +0000

    빵집 문제 풀이 중 - 왜 탭 오류가 나는지도 모르겠고, 문제는 왜 안 풀리는지도 모르겠고 미치겠네 쉬운거 같은데

--- 백준_그리디/빵집.py
+++ 백준_그리디/빵집.py
@@ -3,7 +3,7 @@ def checkUpRight(m, v, rows, cols, r, c) :
 	# 방문한 적이 없어야 하고
 	# X가 아니어야 함
 	# 방향은 r-1, c+1
-	if (r-1) > 0 and (c+1) < cols :
+	if (r-1) >= 0 and (c+1) < cols :
 		if v[r-1][c+1] == 0 :
 			if m[r-1][c+1] != "x" :
 				return True
@@ -50,33 +50,20 @@ def move(i, j, dir) :
 		return (i+1, j+1)
 
 def solution(file) :
-	for i in range(2) :
-		# input 
-		rows, cols = map(int, file.readline().split())
-		matrix = [list(file.readline().strip("\n")) for _ in range(rows)]
-		answer = int(file.readline())
-		file.readline()
-
-		print(rows, cols, answer)
-		for m in matrix :
-			print(m)
-
-		# logic
+	for _ in range(2, 3) :
+		rows, cols = map(int, input().split())
+		matrix = [list(input().strip("\n")) for _ in range(rows)]
+		
 		count = 0
 		visited = [[0 for _ in range(cols)] for _ in range(rows)]
 		for row in range(rows) :
-			print("row : ", row)
-			# 시작점이라도 방문을 했으면 다음으로 넘어감
 			if visited[row][0] != 0 :
-				print("방문한 시작점임")
 				continue
 			else : 
 				path = [(row, 0)]
 				r, c= row, 0
 				isComp = True
-				while c < cols : 
-					print("r, c : ",r, c)
-					# 위 & 오 확인
+				while c < cols - 1 : 
 					if checkUpRight(matrix, visited, rows, cols, r, c) :
 						r, c = move(r, c, "up_right")
 						path.append((r, c))
@@ -92,14 +79,13 @@ def solution(file) :
 						break
 				if isComp :
 					for p in path : 
-						visited[p[0], p[1]] = 1
+						visited[p[0]][p[1]] = 1
 					count += 1
-		print(count)
-		print()
 		
+		print(count)
 
 def main() :
-	file = open("./백준_그리디/빵집tc.txt", "r")
+	file = open("./백준_그리디/빵집tc.txt")
 	print("solution : ", solution(file))
 	file.close()
 
