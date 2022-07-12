1ae7fa544aa908aeb0147ac7dd2a5a0bff90d248
SY97P <>
2022-07-12 08:08:32 +0000

    virus prob is done

--- dfs/바이러스.py
+++ dfs/바이러스.py
@@ -3,10 +3,21 @@ file = open("./dfs/바이러스tc.txt", "r")
 def dfs(node, visited) : 
 	# 종료조건 없어도 될 것 같음
 
+	# 재귀
+	# 연결되어 있으면서 방문하지 않은 곳으로 이동
+	for ed in edges[node] : 
+		if not visited[ed] :
+			visited[ed] = True
+			dfs(ed, visited)
+
 for _ in range(1) : 
 	v = int(file.readline())
 	e = int(file.readline())
-	edges = [list(map(int, file.readline().split())) for _ in range(e)]
+	edges = [[] for _ in range(v+1)]
+	for _ in range(e) : 
+		x, y = map(int, file.readline().split())
+		edges[x].append(y)
+		edges[y].append(x)
 	answer = int(file.readline())
 	file.readline()
 
@@ -14,11 +25,40 @@ for _ in range(1) :
 	for edg in edges : print(edg)
 	print()
 
-	visited = [False for _ in range(n+1)]
+	visited = [False for _ in range(v+1)]
+	visited[0] = visited[1] = True
 
 	dfs(1, visited)
 
 	print(visited)
+
+	print(visited.count(True) - 2)
 	
 
 file.close()
+
+# 백준 제출용
+# import sys
+
+# def dfs(node, visited) : 
+# 	# 종료조건 필요 없어
+
+# 	# 재귀
+# 	for ed in edges[node] : 
+# 		if not visited[ed] :
+# 			visited[ed] = True
+# 			dfs(ed, visited)
+
+# v = int(sys.stdin.readline())
+# e = int(sys.stdin.readline())
+# edges = [[] for _ in range(v+1)]
+# for _ in range(e) : 
+# 	x, y = map(int, sys.stdin.readline().split())
+# 	edges[x].append(y)
+# 	edges[y].append(x)
+# visited = [False for _ in range(v+1)]
+# visited[0] = visited[1] = True
+
+# dfs(1, visited)
+
+# print(visited.count(True) - 2)
