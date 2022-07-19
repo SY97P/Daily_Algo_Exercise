b7fd6d56a45f23148547cd205436e53425c4bd1d
SY97P <>
2022-07-19 05:16:44 +0000

    alphabet prob ondoing

--- dfs/알파벳.py
+++ dfs/알파벳.py
@@ -1,18 +1,23 @@
 # 해결방안 1번 :
 # DFS
+from collections import deque
+
+
 file = open("./dfs/알파벳tc.txt", "r")
 
-def dfs(i, j, path) :
+def dfs(i, j, alpha) :
 	global result
-	print(i, j, path)
 	for d in dx : 
 		di = i + d[0]
 		dj = j + d[1]
-		if 0 <= di < r and 0 <= dj < c and matrix[di][dj] not in path :
-			path += matrix[di][dj]
-			dfs(di, dj, path)
-			path = path[:len(path)-1]
-	result = max(result, len(path))
+		if 0 <= di < r and 0 <= dj < c and not alpha[ord(matrix[di][dj]) - 65] :
+			alpha[ord(matrix[di][dj]) - 65] = True
+			dfs(di, dj, alpha)
+			alpha[ord(matrix[di][dj]) - 65] = False
+	print(result, alpha.count(True))
+	if alpha.count(True)== 4:
+		print(alpha)
+	result = max(result, alpha.count(True))
 
 for _ in range(3) : 
 	r, c = map(int, file.readline().split())
@@ -25,10 +30,37 @@ for _ in range(3) :
 		print(mat)
 
 	result = 0
+	alpha = [False for _ in range(26)]
 	dx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-	dfs(0, 0, [matrix[0][0]])
+	dfs(0, 0, alpha)
 	print(result)
 
 	print()
 
 file.close()
+
+# 백준 제출용
+# import sys
+# from collections import deque
+
+# sys.setrecursionlimit(10 ** 9)
+
+# def dfs(i, j, path) :
+# 	global result
+# 	for d in dx : 
+# 		di = i + d[0]
+# 		dj = j + d[1]
+# 		if 0 <= di < r and 0 <= dj < c and not alpha[ord(matrix[di][dj]) - 65] :
+# 			alpha[ord(matrix[di][dj])] = True
+# 			dfs(di, dj, path)
+# 			alpha[ord(matrix[di][dj])] = False
+# 	result = max(result, len(path))
+
+# r, c = map(int, stdin.readline().split())
+# matrix = [list(stdin.readline().strip("\n")) for _ in range(r)]
+
+# result = 0
+# alpha = [False for _ in range(26)]
+# dx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+# dfs(0, 0, alpha)
+# print(result)
