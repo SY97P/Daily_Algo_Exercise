44dd9a0c2c7b4ea9af63cf67fdffb0e768335fc0
SY97P <onetuks@naver.com>
2022-08-01 12:05:29 +0000

    icebug prob 1st fail

--- dfs/내리막길.py
+++ dfs/내리막길.py
@@ -1,7 +1,82 @@
 # 해결방안 3번 : 
+# DFS + DP (bot top)
+file = open("./dfs/내리막길tc.txt", "r")
 
+def dfs(i, j) : 
+	# 종료조건
+	if i == m - 1 and j == n - 1:
+		return 1
 
-# # 해결방안 2번 : 
+	# dp값 존재
+	if dp[i][j] != -1 : 
+		return dp[i][j]
+
+	# 현재노드에서 상하좌우로 목적지까지 갈 수 있는 총 경우의 수
+	ways = 0
+	for d in dx : 
+		di = i + d[0]
+		dj = j + d[1]
+		if 0 <= di < m and 0 <= dj < n and matrix[i][j] > matrix[di][dj] :
+			ways += dfs(di, dj)
+
+	# 모든 방향 탐색 후 현재 노드 dp값 갱신
+	dp[i][j] = ways
+	return dp[i][j]
+	
+
+m, n = map(int, file.readline().split())
+matrix = [list(map(int, file.readline().split())) for _ in range(m)]
+answer = int(file.readline())
+
+print(m, n, answer)
+for mat in matrix : 
+	print(mat)
+
+# -1로 초기화해야 메모그레이션 제대로 쓰는 것
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+dx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+print(dfs(0, 0))
+
+file.close()
+
+# 백준 제출용
+# 해결방안 3번 : 
+# DFS + DP (bot top)
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+
+def dfs(i, j) : 
+	# 종료조건
+	if i == m - 1 and j == n - 1:
+		return 1
+
+	# dp값 존재
+	if dp[i][j] != -1 : 
+		return dp[i][j]
+
+	# 현재노드에서 상하좌우로 목적지까지 갈 수 있는 총 경우의 수
+	ways = 0
+	for d in dx : 
+		di = i + d[0]
+		dj = j + d[1]
+		if 0 <= di < m and 0 <= dj < n and matrix[i][j] > matrix[di][dj] :
+			ways += dfs(di, dj)
+
+	# 모든 방향 탐색 후 현재 노드 dp값 갱신
+	dp[i][j] = ways
+	return dp[i][j]
+	
+
+m, n = map(int, sys.stdin.readline().split())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+
+# -1로 초기화해야 메모그레이션 제대로 쓰는 것
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+dx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+print(dfs(0, 0))
+
+# # 해결방안 2번 : 시간초과 20%
 # # DFS + DP
 # file = open("./dfs/내리막길tc.txt", "r")
 
