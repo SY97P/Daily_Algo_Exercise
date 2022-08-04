d2703179d89f96a6e3ca883a06e8cdf04a4120b9
SY97P <onetuks@naver.com>
2022-08-04 08:11:16 +0000

    radius of tree prob dp + dfs fail : time out

--- dfs/트리의지름.py
+++ dfs/트리의지름.py
@@ -1,31 +1,70 @@
+# DFS
+# 이진 트리 아니란다!!
 file = open("./dfs/트리의지름tc.txt", "r")
 
-n = int(file.readline())
-adj = [[] for _ in range(n+1)]
-for _ in range(n-1) :
-	p, c, w = map(int, file.readline().split())
-	adj[p].append((c, w))
-	adj[c].append((p, w))
-answer = int(file.readline())
+for tc in range(8) :
+	n = int(file.readline())
+	adj = [[] for _ in range(n+1)]
+	dp = [dict() for _ in range(n+1)]
+	for _ in range(n-1) :
+		p, c, w = map(int, file.readline().split())
+		adj[p].append((c, w))
+		dp[p][c] = 0
+		adj[c].append((p, w))
+		dp[c][p] = 0
+	answer = int(file.readline())
+	file.readline()
+	
+	print(n, answer)
+	for ad in adj :
+		print(ad)
+	
+	maxValue = 0
+	
+	def dfs(info, visited) :
+		pre, now, weight = info
 
-print(n, answer)
-for ad in adj : 
-	print(ad)
+		if dp[pre][now] != 0 :
+			return dp[pre][now]
 
-maxValue = 0
+		visited[now] = True
 
-# 1. 모든 노드를 기준으로
-for node in range(1, n) :
-	# 1 dpth는 모든 방향 DFS
-	curr = [] # 여러개가 있어도 두개만 골라야 함
-	visited = [False for i in range(n+1)]
-	visited[node] = True
-	for ad in adj[node] :
-		visited[ad[0]] = True
-		curr.append(dfs(ad, visited))
-		visited[ad[1]] = False
+		dp_value = 0
+		for ad in adj[now] :
+			n, w = ad
+			if not visited[n] :
+				rtn = dfs((now, n, w), visited)
+				if dp_value < rtn :
+					dp_value = rtn
+					
+		dp[pre][now] = dp_value + weight
+		return dp[pre][now]
+		
+	
+	# 모든 노드를 기준으로 지름을 만들어야 함
+	for node in range(1, n+1) :
 
-	# 최대값은 무엇인고
-	if maxValue < sum(curr[:2])
+		visited = [False for _ in range(n+1)]
+		visited[node] = True
+		
+		# 1 depth에서는 모든 방향을 다 가봐야 함. 
+		weights = []
+		for ad in adj[node] :
+			next, weight = ad
+			# 가려는 방향의 dp값이 있으면 그냥 가지고 있으면 됨
+			weights.append(dfs((node, next, weight), visited))
+
+		weights.sort(reverse = True)
+		
+		if len(weights) < 2: 
+			if maxValue < sum(weights) :
+				maxValue = sum(weights)
+		else : 
+			if maxValue < sum(weights[:2]) :
+				maxValue = sum(weights[:2])
+			
+		
+	print("maxValue : ", maxValue)
+	
 
 file.close()
