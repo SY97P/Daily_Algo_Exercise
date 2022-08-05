a7fa6f00171cfda5a888d41057d59178b18cf4f5
SY97P <onetuks@naver.com>
2022-08-05 10:07:29 +0000

    radius of tree solved

--- dfs/트리의지름.py
+++ dfs/트리의지름.py
@@ -1,70 +1,89 @@
-# DFS
-# 이진 트리 아니란다!!
 file = open("./dfs/트리의지름tc.txt", "r")
 
-for tc in range(8) :
+# 트리 지름 구하는 법 
+# 1. root에서 가장 먼 노드 t 찾기
+# 2. t에서 가장 먼 노드 u 찾기
+# 3. t - u 거리 구하기
+def dfs(node, sumof) : 
+	global node_result
+	global weight_result
+	
+	visited[node] = True
+	for ad in adj[node] : 
+		if not visited[ad[0]] :
+			dfs(ad[0], ad[1] + sumof)
+	if weight_result < sumof :
+		weight_result = sumof
+		node_result = node
+
+for tc in range(8) : 
 	n = int(file.readline())
 	adj = [[] for _ in range(n+1)]
-	dp = [dict() for _ in range(n+1)]
 	for _ in range(n-1) :
 		p, c, w = map(int, file.readline().split())
 		adj[p].append((c, w))
-		dp[p][c] = 0
 		adj[c].append((p, w))
-		dp[c][p] = 0
+
 	answer = int(file.readline())
 	file.readline()
-	
+
 	print(n, answer)
-	for ad in adj :
-		print(ad)
-	
-	maxValue = 0
-	
-	def dfs(info, visited) :
-		pre, now, weight = info
 
-		if dp[pre][now] != 0 :
-			return dp[pre][now]
+	# 1번 
+	visited = [False for _ in range(n+1)]
+	node_result, weight_result = 0, 0
+	dfs(1, 0)
+	print(node_result, weight_result)
 
-		visited[now] = True
+	visited = [False for _ in range(n+1)]
+	node_t = node_result
+	node_result, weight_result = 0, 0
+	dfs(node_t, 0)
+	print(node_result, weight_result)
 
-		dp_value = 0
-		for ad in adj[now] :
-			n, w = ad
-			if not visited[n] :
-				rtn = dfs((now, n, w), visited)
-				if dp_value < rtn :
-					dp_value = rtn
-					
-		dp[pre][now] = dp_value + weight
-		return dp[pre][now]
-		
-	
-	# 모든 노드를 기준으로 지름을 만들어야 함
-	for node in range(1, n+1) :
-
-		visited = [False for _ in range(n+1)]
-		visited[node] = True
-		
-		# 1 depth에서는 모든 방향을 다 가봐야 함. 
-		weights = []
-		for ad in adj[node] :
-			next, weight = ad
-			# 가려는 방향의 dp값이 있으면 그냥 가지고 있으면 됨
-			weights.append(dfs((node, next, weight), visited))
-
-		weights.sort(reverse = True)
-		
-		if len(weights) < 2: 
-			if maxValue < sum(weights) :
-				maxValue = sum(weights)
-		else : 
-			if maxValue < sum(weights[:2]) :
-				maxValue = sum(weights[:2])
-			
-		
-	print("maxValue : ", maxValue)
+	print()
 	
 
 file.close()
+
+# 백준 제출용
+# import sys
+
+
+# sys.setrecursionlimit(10 ** 5)
+
+# # 트리 지름 구하는 법 
+# # 1. root에서 가장 먼 노드 t 찾기
+# # 2. t에서 가장 먼 노드 u 찾기
+# # 3. t - u 거리 구하기
+# def dfs(node, sumof) : 
+# 	global node_result
+# 	global weight_result
+	
+# 	visited[node] = True
+# 	for ad in adj[node] : 
+# 		if not visited[ad[0]] :
+# 			dfs(ad[0], ad[1] + sumof)
+# 	if weight_result < sumof :
+# 		weight_result = sumof
+# 		node_result = node
+	
+
+# n = int(sys.stdin.readline())
+# adj = [[] for _ in range(n+1)]
+# for _ in range(n-1) :
+# 	p, c, w = map(int, sys.stdin.readline().split())
+# 	adj[p].append((c, w))
+# 	adj[c].append((p, w))
+
+
+# # 1번 
+# visited = [False for _ in range(n+1)]
+# node_result, weight_result = 0, 0
+# dfs(1, 0)
+
+# visited = [False for _ in range(n+1)]
+# node_t = node_result
+# node_result, weight_result = 0, 0
+# dfs(node_t, 0)
+# print(node_result, weight_result)
