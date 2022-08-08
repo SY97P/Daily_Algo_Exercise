4f73d0aaf8721c46d7f8883df700b2e2ab749f37
SY97P <onetuks@naver.com>
2022-08-08 13:10:36 +0000

    greedy panda prob solved

--- dfs/트리의지름2.py
+++ dfs/트리의지름2.py
@@ -50,43 +50,43 @@ for tc in range(8) :
 file.close()
 
 # 백준 제출용
-# 트리 지름 구하는 방법 이용
-import sys
+# # 트리 지름 구하는 방법 이용
+# import sys
 
-sys.setrecursionlimit(10 ** 5)
+# sys.setrecursionlimit(10 ** 5)
 
-def dfs(node, sumof) : 
-	global far_node
-	global far_weight
+# def dfs(node, sumof) : 
+# 	global far_node
+# 	global far_weight
 	
-	visited[node] = True
+# 	visited[node] = True
 
-	for ad in adj[node] :
-		n, w = ad
-		if not visited[n] :
-			dfs(n, sumof + w)
+# 	for ad in adj[node] :
+# 		n, w = ad
+# 		if not visited[n] :
+# 			dfs(n, sumof + w)
 
-	if sumof > far_weight : 
-		far_weight = sumof
-		far_node = node
+# 	if sumof > far_weight : 
+# 		far_weight = sumof
+# 		far_node = node
 
-v = int(sys.stdin.readline())
-adj = [[] for _ in range(v+1)]
-for node in range(v) :
-	line = list(map(int, sys.stdin.readline().split()))
-	node = line[0]
-	for i in range(1, len(line)-1, 2) :
-		adj[node].append((line[i], line[i+1]))
+# v = int(sys.stdin.readline())
+# adj = [[] for _ in range(v+1)]
+# for node in range(v) :
+# 	line = list(map(int, sys.stdin.readline().split()))
+# 	node = line[0]
+# 	for i in range(1, len(line)-1, 2) :
+# 		adj[node].append((line[i], line[i+1]))
 
-# 트리에서 가장 먼 노드 t 구하기
-visited = [False for _ in range(v + 1)]
-far_node = 0
-far_weight = 0
-dfs(1, 0)
+# # 트리에서 가장 먼 노드 t 구하기
+# visited = [False for _ in range(v + 1)]
+# far_node = 0
+# far_weight = 0
+# dfs(1, 0)
 
-visited = [False for _ in range(v + 1)]
-t_node = far_node
-far_node, far_weight = 0, 0
-dfs(t_node, 0)
+# visited = [False for _ in range(v + 1)]
+# t_node = far_node
+# far_node, far_weight = 0, 0
+# dfs(t_node, 0)
 
-print(far_weight)
+# print(far_weight)
