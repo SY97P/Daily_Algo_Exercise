82b95d364d441056db4b57a8deb539eaaf145473
SY97P <onetuks@naver.com>
2022-08-16 12:59:10 +0000

    longest increasing subsequence prob dp O(n^2) solution

--- 이진탐색/가장긴증가하는부분수열2.py
+++ 이진탐색/가장긴증가하는부분수열2.py
@@ -1,91 +1,39 @@
 file = open("./이진탐색/가장긴증가하는부분수열2tc.txt", "r")
 
-for tc in range(1) : 
+for _ in range(3) : 
 	n = int(file.readline())
 	nlist = list(map(int, file.readline().split()))
 	answer = int(file.readline())
-	
+	file.readline()
+
 	print(n, answer)
 	print(nlist)
-	
-	nset = sorted(list(set(nlist)))
-	print(nset)
-	
-	def getlen(index) : 
-		curr = nlist[index]
-		count = 1
-		for i in range(index + 1, n) :
-			if curr < nlist[i] :
-				count += 1
-				curr = nlist[i]
-		return count
-		
-	
-	left, right = 0, len(nset)
-	maxlen = 0
-	while left <= right : 
-		mid = (left + right) // 2
-	
-		# 해당 좌표로 수열 만들기
-		count = 0
-		for i, nl in enumerate(nlist) : 
-			if nset[mid] == nl : 
-				temp = getlen(i)
-				print("시작값 , 길이 : ", nl, temp)
-				if count < temp :
-					count = temp
-	
-		# 크네
-		if count > maxlen : 
-			right = mid - 1
-			maxlen = count
-		else : 
-			left = mid + 1
-	
-	print(maxlen)
-	
+
+	dp = [1 for _ in range(n)]
+
+	for i, nl in enumerate(nlist) :
+		for j in range(i) :
+			if nl > nlist[j] : 
+				dp[i] = max(dp[i], dp[j] + 1)
+
+	print(dp)
+	print(max(dp))
 
 file.close()
 
 # 백준 제출용
 import sys
 
+sys.setrecursionlimit(10 ** 9)
+
 n = int(sys.stdin.readline())
 nlist = list(map(int, sys.stdin.readline().split()))
 
-nset = sorted(list(set(nlist)))
-print(nset)
+dp = [1 for _ in range(n)]
 
-def getlen(index) : 
-	curr = nlist[index]
-	count = 1
-	for i in range(index + 1, n) :
-		if curr < nlist[i] :
-			count += 1
-			curr = nlist[i]
-	return count
-	
+for i, nl in enumerate(nlist) :
+	for j in range(i) :
+		if nl > nlist[j] : 
+			dp[i] = max(dp[i], dp[j] + 1)
 
-left, right = 0, len(nset)
-maxlen = 0
-while left <= right : 
-	mid = (left + right) // 2
-
-	# 해당 좌표로 수열 만들기
-	count = 0
-	for i, nl in enumerate(nlist) : 
-		if nset[mid] == nl : 
-			temp = getlen(i)
-			# print("시작값 , 길이 : ", nl, temp)
-			if count < temp :
-				count = temp
-
-	# 크네
-	if count > maxlen : 
-		right = mid - 1
-		maxlen = count
-	else : 
-		left = mid + 1
-
-print(maxlen)
-	
+print(dp)
