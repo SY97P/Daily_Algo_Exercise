88c40d426c89c79f36a97f90f0cbe3846ce67316
SY97P <onetuks@naver.com>
2022-09-06 10:35:52 +0000

    airport prob ondoing

--- 백준_그리디/공항.py
+++ 백준_그리디/공항.py
@@ -1,263 +1,47 @@
-<<<<<<< HEAD
-# def checkPlane(queue, index) :
-# 	temp = index - 1
-# 	while temp >= 0 and queue[temp] == 0 :
-# 		temp -= 1
-# 	if temp < 0 :
-# 		# 이전 값 중에 0이 아닌 값이 없는것# 
-# 		# 현재 인덱스 + 1 보다 큰 값이 있는거 아니면 참 반환
-# 		if index + 1 < queue[index] :
-# 			return False
-# 		else :
-# 			return True
-# 	else :
-# 		# 이전 값 중 0인 값이 있음 (temp 인덱스 값)
-# 		# 해당 값이랑 비교해서 index의 값이 index # - temp 보다 크면 거짓 반환
-# 		if queue[index] > index - temp :
-# 			return False
-# 		else :
-# 			return True
-
-def find_parent(dic, x) :
-	if dic[x] == x :
-		return x
-	value = find_parent(parent[x])
-	parent[x] = value
-	
-
-def solution(file) :
-	for _ in range(4) :
-		g = int(file.readline().strip('\n'))
-		p = int(file.readline().strip('\n'))
-		planes = [int(file.readline().strip('\n')) for _ in range(p)]
-		answer = int(file.readline().strip('\n'))
-		file.readline()
-
-		print(g, p, planes, answer)
-
-		# gates = [False for _ in range(g)]
-		# count = 0
-
-		# for pl in planes :
-		# 	try :
-		# 		# reversed list 는 reverse_iterator 객체를 반환
-		# 		# 해당 객체는 index 라이브러리가 없음
-		# 		# 이런식으로 list type으로 형변환을 해줘야 함
-		# 		lst = list(reversed(gates[:pl]))
-		# 		index = lst.index(False)
-		# 		gates[pl-1-index] = True
-		# 		count += 1
-		# 	except :
-		# 		break
-
-		# 시간초과 해결 방안
-		# 이렇게 하면 틀리다고 나음.
-		# queue = [0 for _ in range(g)]
-		# count = 0
-		# for pl in planes :
-		# 	queue[pl-1] += 1
-		# 	if checkPlane(queue, pl-1) : 
-		# 		count += 1
-		# 	else : 
-		# 		break
-		# print(count)
-
-		# union-find algorithm
-		parent = {i : i for i in range(g+1)}
-		count = 0
-
-		for pl in planes :
-			# 현재 비행기의 부모를 구함
-			# 구한 부모가 0보다 작거나 같으면 폐쇄조건
-			# 그렇지 않으면 count 를 증가해주면 된다.s
-			value = find_parent(pl)
-
-			if value <= 0 :
-				break
-			union(value , value - 1)
-			count += 1
-		print(count)
-
-# 백준 제출용
-
-=======
-# 아예 새로
-# 전역변수 쓰고 싶음
-
-def find_parent(x) :
-	if parent[x] == x :
-		return x
-	P = find_parent(parent[x])
-	parent[x] = P
-	return parent[x]
-
-def union(x, y) :
-	x = find_parent(x)
-	y = find_parent(y)
-	if x < y :
-		parent[y] = x
-	else :
-		parent[x] = y
-
 file = open("./백준_그리디/공항tc.txt", "r")
 
-for _ in range(2) :
-	g = int(file.readline().strip("\n"))
-	p = int(file.readline().strip("\n"))
-	planes = [int(file.readline().strip("\n")) for _ in range(p)]
-	answer = int(file.readline().strip("\n"))
+def union(curr_index, prev_index) : 
+	print(curr_index, prev_index)
+	curr = parent(curr_index)
+	prev = parent(prev_index)
+	print(curr, prev)
+	# 낮은 급수의 공항이 더 수용량이 많으면 낮은 급수의 수용량을 현재 수용량으로 맞춰줌 (이미 현재 공항에는 한 대가 온 상황이므로 전 공항 수용량과 같아야 함)
+	if curr < prev : 
+		gates[prev] = curr
+	elif curr > prev :
+		gates[curr] = prev
+	else :
+		union(curr_index - 1, prev_index)
+
+def parent(item) : 
+	if item != gates[item] :
+		gates[item] = parent(gates[item])
+	return gates[item]
+
+for tc in range(4) :
+	g = int(file.readline())
+	p = int(file.readline())
+	planes = [int(file.readline()) for _ in range(p)]
+	answer = int(file.readline())
 	file.readline()
 
-	print(g, p, planes, answer)
+	print(g, p, " / ", answer)
+	print(planes)
 
-	parent = {i : i for i in range(g+1)}
-	count = 0 
+	gates = {i : i for i in range(g+1)}
 
-	for pl in planes : 
-		if find_parent(pl) == 0 :
+	step = 0
+
+	for plane in planes : 
+		if parent(plane) == 0 :
 			break
-		union(pl, pl - 1)
-		count += 1
-	print(count)
+		print(gates)
+		union(plane, plane - 1)
+		step += 1
+
+	print(step)
+	print()
 
 file.close()
 
-
-
-# # def checkPlane(queue, index) :
-# # 	temp = index - 1
-# # 	while queue[temp] == 0 :
-# # 		if temp < 0 :
-# # 			break
-# # 		temp -= 1
-# # 	if temp < 0 :
-# # 		# 이전 값 중에 0이 아닌 값이 없는것
-# # 		# 현재 인덱스 + 1 보다 큰 값이 있는거 아니면 참 반환
-# # 		if index + 1 < queue[index] :
-# # 			return False
-# # 		else :
-# # 			return True
-# # 	else :
-# # 		# 이전 값 중 0인 값이 있음 (temp 인덱스 값)
-# # 		# 해당 값이랑 비교해서 index의 값이 index - temp 보다 크면 거짓 반환
-# # 		if queue[index] > index - temp :
-# # 			return False
-# # 		else :
-# # 			return True
-
-# # dict에서 해당 x에 대한 부모를 계속 타고 들어가서 최종 부모의 값을 가져옴
-# def find_parent(parent, x) :
-# 	# 종료조건
-# 	# 현재 값과 부모의 값이 같으면 아직 들어갈 공간이 남았다는 것
-# 	if parent[x] == x :
-# 		return x
-# 	# 부모를 통해서 받아온 값을 넣어줌
-# 	P = find_parent(parent, parent[x])
-# 	parent[x] = P
-# 	return parent[x]
-
-# def union(parent, x, y) :
-# 	x = find_parent(parent, x)
-# 	y = find_parent(parent, y)
-# 	if x < y :
-# 		parent[y] = x
-# 	else : 
-# 		parent[x] = y
-
-# def solution(file) :
-# 	for _ in range(2) :
-# 		g = int(file.readline().strip('\n'))
-# 		p = int(file.readline().strip('\n'))
-# 		planes = [int(file.readline().strip('\n')) for _ in range(p)]
-# 		answer = int(file.readline().strip('\n'))
-# 		file.readline()
-
-# 		print(g, p, planes, answer)
-
-# 		# gates = [False for _ in range(g)]
-# 		# count = 0
-
-# 		# for pl in planes :
-# 		# 	try :
-# 		# 		# reversed list 는 reverse_iterator 객체를 반환
-# 		# 		# 해당 객체는 index 라이브러리가 없음
-# 		# 		# 이런식으로 list type으로 형변환을 해줘야 함
-# 		# 		lst = list(reversed(gates[:pl]))
-# 		# 		index = lst.index(False)
-# 		# 		gates[pl-1-index] = True
-# 		# 		count += 1
-# 		# 	except :
-# 		# 		break
-
-# 		# 시간초과 해결 방안
-# 		# 이 방법은 오답 발생
-# 		# queue = [0 for _ in range(g)]
-# 		# count = 0
-# 		# for pl in planes :
-# 		# 	queue[pl-1] += 1
-# 		# 	if checkPlane(queue, pl-1) : 
-# 		# 		count += 1
-# 		# 	else : 
-# 		# 		break
-# 		# print(count)
-
-# 		# union-find 알고리즘 사용
-# 		parent = {i : i for i in range(g)}
-# 		count = 0
-
-# 		for pl in planes : 
-# 			x = find_parent(parent, pl)
-
-# 			if x == 0 :
-# 				break
-# 			union(parent, x, x - 1)
-# 			count += 1
-# 		print(count)
-			
-
-# # 백준 제출용
-# # import sys
-
-# # def checkPlane(queue, index) :
-# # 	temp = index - 1
-# # 	while queue[temp] == 0 :
-# # 		if temp < 0 :
-# # 			break
-# # 		temp -= 1
-# # 	if temp < 0 :
-# # 		# 이전 값 중에 0이 아닌 값이 없는것
-# # 		# 현재 인덱스 + 1 보다 큰 값이 있는거 아니면 참 반환
-# # 		if index + 1 < queue[index] :
-# # 			return False
-# # 		else :
-# # 			return True
-# # 	else :
-# # 		# 이전 값 중 0인 값이 있음 (temp 인덱스 값)
-# # 		# 해당 값이랑 비교해서 index의 값이 index - temp 보다 크면 거짓 반환
-# # 		if queue[index] > index - temp :
-# # 			return False
-# # 		else :
-# # 			return True
-			
-# # g = int(sys.stdin.readline().strip('\n'))
-# # p = int(sys.stdin.readline().strip('\n'))
-# # planes = [int(sys.stdin.readline().strip('\n')) for _ in range(p)]
-
-# # queue = [0 for _ in range(g)]
-# # count = 0 
-
-# # for pl in planes :
-# # 	queue[pl-1] += 1
-# # 	if checkPlane(queue, pl - 1) :
-# # 		count += 1
-# # 	else :
-# # 		break
-# # print(count)
->>>>>>> 108997e6663a7a302df30f2f795fbc7d79117940
-
-# def main() :
-# 	file = open("./백준_그리디/공항tc.txt", "r")
-# 	print("solution : ", solution(file))
-# 	file.close()
-
-# main()
+# 백준 제출용
