87fe0f0f5b2e3bd9ad44dcbaa0714048e64f3818
SY97P <onetuks@naver.com>
2022-10-31 14:37:51 +0000

    hanaro prob solved

--- SWEA/D4/하나로.py
+++ SWEA/D4/하나로.py
@@ -23,19 +23,63 @@ file = open("./SWEA/D4/하나로.txt", "r")
 
 input = file.readline
 
+from collections import defaultdict
+import heapq
+
 t = int(input())
+
+def getTax(dist) : 
+	return rate * (dist ** 2)
+
+def getCandi(node) : 
+	global n
+	
+	result = []
+	
+	curr_x, curr_y = coor[node]
+
+	for i in range(n) : 
+		if not visited[i] : 
+			next_x, next_y = coor[i]
+			dist = (((curr_x - next_x) ** 2) + ((curr_y - next_y) ** 2)) ** (1/2)
+			result.append((dist, i))
+
+	return result
+
+def prim(node) : 
+	visited[node] = True
+
+	candi = getCandi(node)
+	heapq.heapify(candi)
+
+	total_tax = 0
+
+	while candi : 
+		dist, next = heapq.heappop(candi)
+
+		if not visited[next] : 
+			visited[next] = True
+			total_tax += getTax(dist)
+
+			temp = getCandi(next)
+			for i in range(len(temp)) : 
+				heapq.heappush(candi, temp[i])
+				
+	return total_tax
 
 for tc in range(1, t + 1) : 
 	n = int(input())
 	coor_x = list(map(int, input().split()))
 	coor_y = list(map(int, input().split()))
-	tax = float(input())
-	
+	rate = float(input())
+
+	# coor = [(node, x, y)]
 	coor = []
-	for i in range(n) :
+	for i in range(n) : 
 		coor.append((coor_x[i], coor_y[i]))
 
-	print(coor)
-	print(tax)
+	visited = [False for _ in range(n)]
+
+	print("#%d %d" % (tc, round(prim(0))))
 
 file.close()
