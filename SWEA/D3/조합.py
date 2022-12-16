4749217bfb6f209ad47c7d4f8ba0c3a9ad27543c
Seyoung Park <54990890+SY97P@users.noreply.github.com>
2022-12-16 03:45:39 +0000

    [D3]재미있는 오셀로 게임 풀이 완료

--- SWEA/D3/조합.py
+++ SWEA/D3/조합.py
@@ -4,23 +4,32 @@ file = open("./SWEA/D3/조합.txt", "r")
 
 input = file.readline
 
+import math
+
 t = int(input())
+
+# 2^5 = 2^2 * 2^2 * 2^1
+# 2^4 = 2^2 * 2^2
+def pow(a, b) : 
+	global p 
+	
+	if b == 0 : 
+		return 1
+	sub = pow(a, b//2)
+	result = (sub ** 2) % p
+
+	if b % 2 == 0 : 
+		return result
+	else : 
+		return (result * a) % p
 
 for tc in range(1, t + 1) : 
 	n, r = map(int, input().split())
 
-	sumof = 1
+	p = 1234567891
 
-	temp = n
+	result = math.factorial(n) * pow(math.factorial(r) * math.factorial(n-r) % p , p - 2) % p
 
-	while temp > n - r : 
-		sumof *= temp
-		temp -= 1
-	
-	while r > 0 : 
-		sumof //= r
-		r -= 1
-
-	print("#%d %d" %(tc, sumof % 1234567891))
+	print("#%d %d" %(tc, result))
 
 file.close()
