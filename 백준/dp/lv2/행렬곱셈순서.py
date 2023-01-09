ef2a8b7244c9e7d99796f8d0217f2e237a1ff723
Seyoung Park <54990890+SY97P@users.noreply.github.com>
2023-01-09 03:48:06 +0000

    [G3] 행렬 곱셈 순서 - 문제 풀이 완료 - java, pypy3 풀이

--- 백준/dp/lv2/행렬곱셈순서.py
+++ 백준/dp/lv2/행렬곱셈순서.py
@@ -1,26 +1,27 @@
+# 96
+# 31
 # 90 
 
 file = open("./백준/dp/lv2/행렬곱셈순서.txt", "r")
 
 input = file.readline
 
-def getCount() : 
+def solve(n) : 
 	for d in range(1, n) : 
-		for i in range(n-d) : 
+		for i in range(n - d) : 
+			# 0 < j < n
+			# 0 < i + d < n
+			# -d (0) < i < n - d
 			j = i + d
-			dp[i][j] = matrix[i][0] * matrix[j][0] * matrix[j][1]
-			dp[i][j] += min([dp[i][k] + dp[k+1][j] for k in range(i, j)])
-
-	# for d in dp : 
-	# 	print(d)
+			dp[i][j] = min([dp[i][k] + dp[k+1][j] + mat[i][0] * mat[k][1] * mat[j][1] for k in range(i, j)])
 
 	return dp[0][-1]
 
 n = int(input())
-matrix = [tuple(map(int, input().split())) for _ in range(n)]
+mat = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[0 for _ in range(n)] for _ in range(n)]
 
-print(getCount())
+print(solve(n))
 
 file.close()
