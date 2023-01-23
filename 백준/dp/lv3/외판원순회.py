39ad634343adbb64cf6d3847122213813d7023fe
Seyoung Park <54990890+SY97P@users.noreply.github.com>
2023-01-23 09:29:03 +0000

    [G1] 외판원순회 - 문제 풀이 완료

--- 백준/dp/lv3/외판원순회.py
+++ 백준/dp/lv3/외판원순회.py
@@ -4,29 +4,29 @@ file = open("./백준/dp/lv3/외판원순회.txt", "r")
 
 input = file.readline
 
-def dfs(curr, visited, cost, start) : 
-	dp[curr][visited] = cost
+def dfs(node, visited) : 
+	if visited == (1<<n) - 1: 
+		if w[node][0] == 0 : 
+			return float('inf')
+		else : 
+			return w[node][0]
 
-	if visited == (1<<n) - 1 : 
-		print(start, visited)
-		dp[start][visited] += w[curr][start]
-		return
+	if dp[node][visited] != -1 : 
+		return dp[node][visited]
 
-	for i in range(n) : 
-		if not visited & (1<<i) and dp[i][visited|1<<i] > cost + w[curr][i] :
-			dfs(i, visited | 1<<i, cost + w[curr][i], start)
-			
+	dist = float('inf')
+	for i in range(1, n) : 
+		if visited & 1<<i == 0 and w[node][i] != 0 : 
+			dist = min(dist, dfs(i, visited|1<<i) + w[node][i])
+
+	dp[node][visited] = dist
+	return dp[node][visited]
 
 n = int(input())
-w = [list(map(int ,input().split())) for _ in range(n)]
+w = [list(map(int, input().split())) for _ in range(n)]
 
-# dp[curr][visited] : curr 도시에 있을 때 방문상황 visited 에 대한 최소비용
-dp = [[float('inf') for _ in range(1 << n)] for _ in range(n)]
+dp = [[-1] * (1<<n) for _ in range(n)]
 
-for i in range(n) : 
-	dfs(i, 1<<i, 0, i)
-
-for d in dp : 
-	print(d)
+print(dfs(0, 1))
 
 file.close()
