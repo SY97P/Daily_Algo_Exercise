3ed035a5748dae81d8cee8e973504f55ea031242
DESKTOP-FBPVVKP\onetu <onetuks@naver.com>
2023-03-03 11:59:44 +0900

    [G2] 피보나치수6 - 문제 풀이 완료 - 다시 풀어보기 권고

--- 백준/단계별/분할정복/피보나치수6.py
+++ 백준/단계별/분할정복/피보나치수6.py
@@ -9,40 +9,32 @@ def square_matrix(n):
     if n < 2:
         return matrix
     rtn_value = square_matrix(n//2)
-    if n%2 != 0:
-        return multiply_matrix(multiply_matrix(rtn_value, rtn_value), matrix)
-    return multiply_matrix(rtn_value, rtn_value)
+    if n % 2 == 0:
+        return multiply_matrix(rtn_value, rtn_value)
+    return multiply_matrix(multiply_matrix(rtn_value, rtn_value), matrix)
 
 
-def multiply_matrix(mat_a, matrix_b):
-    bound = 2
-    if matrix_b[0] == 1:
-        bound = 1
-    result = [[0 for _ in range(bound)] for _ in range(2)]
-    mat_b = list(zip(*matrix_b)) if bound != 1 else [1, 0]
+def multiply_matrix(mat_a, mat_b):
+    result = [[0 for _ in range(2)] for _ in range(2)]
     for i in range(2):
-        for j in range(bound):
-            result[i][j] = multiply(mat_a[i], mat_b[j], bound)
+        for j in range(2):
+            result[i][j] = multiply(mat_a[i], mat_b[j])
     return result
 
 
-def multiply(sub_a, sub_b, bound):
+def multiply(sub_a, sub_b):
     result = 0
     for i in range(2):
-        if bound == 1:
-            result += sub_a[i] * sub_b
-            continue
         result += sub_a[i] * sub_b[i]
     return result % p
 
 
 def main():
     global matrix, p
-
     p = 1000000007
     n = int(input())
-    matrix = [[1, 1], [1, 0]]
-    print(multiply_matrix(square_matrix(n), matrix[1])[-1][-1] % p)
+    matrix = [[1,1], [1,0]]
+    print(square_matrix(n)[1][0] % p)
 
 
 if __name__ == '__main__':
