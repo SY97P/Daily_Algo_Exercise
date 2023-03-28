6d6775edb78fb10e092618ddd120dc643e4c74f6
Sey0 <54990890+SY97P@users.noreply.github.com>
2023-03-28 04:51:02 +0000

    [투포인터] 합이0 - 재풀이 요망

--- Daily_Algo_Exercise/백준/문제집/투포인터/합이0.py
+++ Daily_Algo_Exercise/백준/문제집/투포인터/합이0.py
@@ -4,18 +4,30 @@ file = open("./Daily_Algo_Exercise/백준/문제집/투포인터/합이0.txt")
 
 input = file.readline 
 
+from collections import Counter
+
 n = int(input())
 alist = list(map(int, input().split()))
 alist.sort()
 
+count = Counter(alist)
+
 result = 0
 for i in range(n-2):
-	l, r = i+1, n-1
+	l, r = i + 1, n - 1
 	while l < r:
 		value = alist[i] + alist[l] + alist[r]
 		if value == 0:
-			result += 1
-		elif value > 0:
-			
+			if alist[l] == alist[r]:
+				result += r - l
+			else:
+				result += count[alist[r]]
+			l += 1
+		elif value < 0:
+			l += 1
+		else:
+			r -= 1
+print(result)
+	
 
 file.close()
