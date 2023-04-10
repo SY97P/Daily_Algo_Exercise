04eeec30ac96d27b0f94afb18d63b2fa13cfae66
Sey0 <54990890+SY97P@users.noreply.github.com>
2023-04-10 03:44:05 +0000

    [그래프이론] 팀결성
    - 유니온파인드 빈출 정리 및
    - 빈출정리 디렉 정리
    - 문제 풀이 완료

---
+++ Daily_Algo_Exercise/이코테/이론/그래프이론/팀결성.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+
+parent = [i for i in range(n+1)]
+
+def find(parent, num):
+	if parent[num] != num:
+		parent[num] = num
+	return parent[num]
+
+def union(parent, a, b):
+	fa = find(parent, a)
+	fb = find(parent, b)
+	if fa < fb:
+		parent[fb] = fa
+	else:
+		parent[fa] = fb
+
+for _ in range(m):
+	o, a, b = map(int, input().split())
+
+	if o == 0:
+		union(parent, a, b)
+	else:
+		if find(parent, a) == find(parent, b):
+			print("YES")
+		else:
+			print("NO")
