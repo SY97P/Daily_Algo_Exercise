1c7ec90575a9b14e88c2b04532d334883300f524
Sey0 <54990890+SY97P@users.noreply.github.com>
2023-04-10 01:59:33 +0000

    [다이나믹] 개미전사
    - 문제 풀이 완료

---
+++ Daily_Algo_Exercise/이코테/이론/다이나믹/개미전사.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+dp = [0] * n
+
+dp[0] = array[0]
+dp[1] = max(array[0:2])
+
+for i in range(2, n):
+	dp[i] = max(dp[i-1], dp[i-2] + array[i])
+
+print(dp[n-1])
