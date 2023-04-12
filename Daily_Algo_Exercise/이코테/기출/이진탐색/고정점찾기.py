46b5b6da87fb7d24f2657c214e029f49a93dfcb7
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-12 05:44:53 +0000

    [이진탐색] 고정점 찾기
    - 문제 풀이 완료

---
+++ Daily_Algo_Exercise/이코테/기출/이진탐색/고정점찾기.py
@@ -0,0 +1,17 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+answer = -1
+
+l, r = 0, n-1
+while l <= r:
+	m = (l + r) // 2
+	if m == array[m]:
+		answer = m
+		break
+	elif m < array[m]:
+		r = m - 1
+	else:
+		l = m + 1
+
+print(answer)
