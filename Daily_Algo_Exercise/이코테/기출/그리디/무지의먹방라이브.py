b241e2d49d5c3db48be42af753134499ebac237f
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-13 07:07:30 +0000

    [그리디] 무지의 먹방 라이브
    - 재풀이 요오망

--- Daily_Algo_Exercise/이코테/기출/그리디/무지의먹방라이브.py
+++ Daily_Algo_Exercise/이코테/기출/그리디/무지의먹방라이브.py
@@ -1,6 +1,22 @@
-food_times = [3,1,2]
+food_times = [3, 1, 2]
 k = 5
 
 
-n = len(food_times)
+import heapq
 
+n = len(food_times)
+q = []
+for i in range(n):
+	heapq.heappush(q, (food_times[i], i+1))
+
+curr_time = 0
+prev_food = 0
+
+while curr_time + (q[0][0] - prev_food) * n <= k:
+	now_food, now_index = heapq.heappop(q)
+	curr_time += (now_food - prev_food) * n
+	prev_food = now_food 
+	n -= 1
+
+result = sorted(q, key=lambda x: x[1])
+print(result[(k-curr_time)%n][1])
