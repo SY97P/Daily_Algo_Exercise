c99db562b59029897b392e1c5d832a6edd7949a8
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 08:07:22 +0000

    [이진탐색] 정렬된 배열에서 특정 수의 개수 구하기
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/이진탐색/정렬된배열에서특정수의개수구하기.py
+++ Daily_Algo_Exercise/이코테/기출/이진탐색/정렬된배열에서특정수의개수구하기.py
@@ -1,35 +1,13 @@
-import bisect
+from bisect import bisect_left, bisect_right
 
 n, x = map(int, input().split())
 array = list(map(int, input().split()))
 
-left_bound = bisect.bisect_left(array, x)
-right_bound = bisect.bisect_right(array, x)
+# n, x = 7, 4
+# array = [1,1,2,2,2,2,3]
 
-print(right_bound - left_bound if right_bound-left_bound != 0 else -1)
-
-# n, x = map(int, input().split())
-# array = list(map(int, input().split()))
-
-# left_bound, right_bound = 0, 0
-
-# l, r = 0, n-1
-# while l <= r:
-# 	m = (l + r) // 2
-# 	if x <= array[m]:
-# 		left_bound = m
-# 		r = m - 1
-# 	else:
-# 		l = m + 1
-
-# l, r = 0, n-1
-# while l <= r:
-# 	m = (l + r) // 2
-# 	if x >= array[m]:
-# 		right_bound = m
-# 		l = m + 1
-# 	else:
-# 		r = m - 1
-
-# answer = right_bound - left_bound + 1
-# print(answer if answer != n else -1)
+# x를 삽입할 가장 왼쪽 인덱스
+left_bound = bisect_left(array, x)
+# x를 삽입할 가장 오른쪽 인덱스
+right_bound = bisect_right(array, x)
+print(right_bound - left_bound if right_bound - left_bound != 0 else -1)
