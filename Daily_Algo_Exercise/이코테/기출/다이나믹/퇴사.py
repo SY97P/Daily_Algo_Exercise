aba352ad0afa74d37da6386d7b0b5c94cdd33cf6
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 09:45:32 +0000

    [다이나믹] 퇴사
    - DP는 현재 까지 연산중에 최적의 값을 계속 가져가야 함.

--- Daily_Algo_Exercise/이코테/기출/다이나믹/퇴사.py
+++ Daily_Algo_Exercise/이코테/기출/다이나믹/퇴사.py
@@ -2,29 +2,24 @@ file = open("./Daily_Algo_Exercise/이코테/기출/다이나믹/퇴사.txt")
 
 input = file.readline 
 
+
 n = int(input())
 t, p = [], []
+dp = [0] * (n+1)
 for _ in range(n):
 	a, b = map(int, input().split())
 	t.append(a)
 	p.append(b)
 
-dp = [0] * (n+1)
-
 max_val = 0
-
 for i in range(n-1, -1, -1):
-	time = t[i] + i
-
-	if time <= n:
-		dp[i] = max(p[i] + dp[time], max_val)
+	if i + t[i] <= n:
+		dp[i] = max(max_val, dp[i+t[i]]) + p[i]
 		max_val = dp[i]
 	else:
 		dp[i] = max_val
 
-print(dp)
-
 print(max_val)
-	
+
 
 file.close()
