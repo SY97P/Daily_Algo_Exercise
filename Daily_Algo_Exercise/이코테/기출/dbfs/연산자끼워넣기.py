50af7d574c336e46d3de5b199310954101f7adab
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 05:35:53 +0000

    [dbfs] 연산자 끼워넣기
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/dbfs/연산자끼워넣기.py
+++ Daily_Algo_Exercise/이코테/기출/dbfs/연산자끼워넣기.py
@@ -3,39 +3,39 @@ file = open("./Daily_Algo_Exercise/이코테/기출/dbfs/연산자끼워넣기.t
 input = file.readline 
 
 
-from collections import deque
-
 n = int(input())
 array = list(map(int, input().split()))
 oper = list(map(int, input().split()))
-min_val, max_val = int(1e9), -int(1e9)
+
+result = [-int(1e9), int(1e9)]
 
 def dfs(idx, val, oper):
-	global min_val, max_val
-	if idx == n-1:
-		min_val = min(min_val, val)
-		max_val = max(max_val, val)
+	if idx >= n-1:
+		result[0] = max(result[0], val)
+		result[1] = min(result[1], val)
 		return
-	for i in range(4):
-		if oper[i] > 0:
+
+	for i, o in enumerate(oper):
+		next_idx = idx + 1
+		if o != 0:
 			oper[i] -= 1
-			next_val = val
 			if i == 0:
-				next_val += array[idx+1]
+				dfs(next_idx, val + array[next_idx], oper)	
 			elif i == 1:
-				next_val -= array[idx+1]
+				dfs(next_idx, val - array[next_idx], oper)
 			elif i == 2:
-				next_val *= array[idx+1]
+				dfs(next_idx, val * array[next_idx], oper)
 			elif i == 3:
-				next_val = next_val // array[idx+1] if next_val >= 0 else -(-next_val // array[idx+1])
-			dfs(idx+1, next_val, oper)
+				next_val = val // array[next_idx]
+				if val < 0:
+					next_val = -(-val // array[next_idx])
+				dfs(next_idx, next_val, oper)
 			oper[i] += 1
-	
 
 dfs(0, array[0], oper)
 
-print(max_val)
-print(min_val)
+for r in result:
+	print(r)
 
 
 file.close()
