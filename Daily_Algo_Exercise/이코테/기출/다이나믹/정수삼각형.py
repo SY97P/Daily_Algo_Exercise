09108b3732d7d4749b9cc3fa95f23feaf62abe0b
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 09:15:24 +0000

    [다이나믹] 정수 삼각형
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/다이나믹/정수삼각형.py
+++ Daily_Algo_Exercise/이코테/기출/다이나믹/정수삼각형.py
@@ -1,12 +1,11 @@
 n = int(input())
-array = [list(map(int, input().split())) for _ in range(n)]
-
-dp = [[0] * i for i in range(1, n+1)]
-dp[0] = array[0]
+matrix = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (i+1) for i in range(n)]
+dp[0][0] = matrix[0][0]
 
 for i in range(n-1):
-	for j in range(len(array[i])):
-		dp[i+1][j] = max(dp[i+1][j], dp[i][j] + array[i+1][j])
-		dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + array[i+1][j+1])
+	for j in range(i+1):
+		dp[i+1][j] = max(dp[i+1][j], dp[i][j] + matrix[i+1][j])
+		dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + matrix[i+1][j+1])
 
 print(max(dp[-1]))
