a813c8395c5bc00f88460f88a7a650018d84cfe1
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 05:49:55 +0000

    [dbfs] 감시 피하기
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/dbfs/감시피하기.py
+++ Daily_Algo_Exercise/이코테/기출/dbfs/감시피하기.py
@@ -2,7 +2,6 @@ file = open("./Daily_Algo_Exercise/이코테/기출/dbfs/감시피하기.txt")
 
 input = file.readline 
 
-from copy import deepcopy
 
 n = int(input())
 matrix = [list(input().split()) for _ in range(n)]
@@ -11,50 +10,58 @@ answer = False
 
 
 def check(matrix):
-	temp = deepcopy(matrix)
-	
 	for i in range(n):
 		for j in range(n):
-			if temp[i][j] == "T":
-				for k in range(1,n):
-					if i+k >= n or matrix[i+k][j] == "O":
-						break
-					if matrix[i+k][j] == "S":
-						# print("남쪽 발견")
-						return False
-				for k in range(1,n):
-					if i-k < 0 or matrix[i-k][j] == "O":
-						break
-					if matrix[i-k][j] == "S":
-						# print("북쪽 발견")
-						return False
-				for k in range(1,n):
-					if j+k >= n or matrix[i][j+k] == "O":
-						break 
-					if matrix[i][j+k] == "S":
-						# print("동쪽 발견")
-						return False
-				for k in range(1,n):
-					if j-k < 0 or matrix[i][j-k] == "O":
-						break 
-					if matrix[i][j-k] == "S":
-						# print("서쪽 발견")
-						return False
+			if matrix[i][j] == "T":
+				for k in range(1, n):
+					# 동
+					if j+k < n:
+						if matrix[i][j+k] == "O":
+							break
+						if matrix[i][j+k] == "S":
+							# print("동")
+							return False
+				for k in range(1, n):
+					# 서 
+					if j-k >= 0:
+						if matrix[i][j-k] == "O":
+							break
+						if matrix[i][j-k] == "S":
+							# print("서")
+							return False
+				for k in range(1, n):
+					# 남
+					if i+k < n:
+						if matrix[i+k][j] == "O":
+							break
+						if matrix[i+k][j] == "S":
+							# print("남")
+							return False
+				for k in range(1, n):
+					# 북
+					if i-k >= 0:
+						if matrix[i-k][j] == "O":
+							break
+						if matrix[i-k][j] == "S":
+							# print("북")
+							return False
 	return True
-						
+
 
 def dfs(count):
 	global answer
-	if count >= 3:
-		answer = answer or check(matrix)
+	if answer:
 		return
+	if count == 3:
+		answer = answer or check(matrix)
+		return 
 	for i in range(n):
 		for j in range(n):
 			if matrix[i][j] == "X":
 				matrix[i][j] = "O"
 				dfs(count + 1)
 				matrix[i][j] = "X"
-	
+
 
 dfs(0)
 
