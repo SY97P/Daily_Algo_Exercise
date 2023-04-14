6d5b71ca12d9a329011b712ba0104501a324ad89
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 08:26:28 +0000

    [이진탐색] 공유기 설치
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/이진탐색/공유기설치.py
+++ Daily_Algo_Exercise/이코테/기출/이진탐색/공유기설치.py
@@ -1,23 +1,26 @@
+# n, c = 5, 3
+# array = [1,2,8,4,9]
+
 n, c = map(int, input().split())
 array = [int(input()) for _ in range(n)]
+
 array.sort()
 
-answer = array[-1] - array[0]
+answer = 0
 
-l, r = array[1] - array[0], array[-1] - array[0]
+l, r = 1, array[-1] + 1
 while l <= r:
 	m = (l + r) // 2
 	count = 1
-	curr = array[0]
+	curr = array[0] + m
 	for i in range(1, n):
-		if curr + m <= array[i]:
-			curr = array[i]
+		if curr <= array[i]:
 			count += 1
-	# print(count, c, m)
-	if count >= c:
+			curr = array[i] + m
+	# print(m, count)
+	if count < c:
+		r = m - 1
+	else:
 		answer = m
 		l = m + 1
-	else:
-		r = m - 1
-
 print(answer)
