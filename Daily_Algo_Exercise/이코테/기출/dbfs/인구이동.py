fd82b637bb02887bb839f45d32a09157cc3c96c9
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-14 06:19:17 +0000

    [dbfs] 인구이동
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/dbfs/인구이동.py
+++ Daily_Algo_Exercise/이코테/기출/dbfs/인구이동.py
@@ -2,47 +2,52 @@ file = open("./Daily_Algo_Exercise/이코테/기출/dbfs/인구이동.txt")
 
 input = file.readline 
 
-from collections import deque
 
 n, l, r = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
+
 d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def bfs(i, j, index):
-	q = deque([(i, j)])
-	united = [(i, j)]
-	summary = matrix[i][j]
-	count = 1
-	union[i][j] = index
-	while q:
-		i, j = q.popleft()
-		for dx, dy in d:
-			di = i + dx 
-			dj = j + dy 
-			if 0 <= di < n and 0 <= dj < n and union[di][dj] == -1:
-				if l <= abs(matrix[di][dj] - matrix[i][j]) <= r:
-					union[di][dj] = index
-					united.append((di, dj))
-					summary += matrix[di][dj]
-					count += 1
-					q.append((di, dj))
-	for x, y in united:
-		matrix[x][y] = summary // count
-	return count
+
+def dfs(i, j, union_num):
+	for dx, dy in d:
+		di = i + dx
+		dj = j + dy
+		if 0 <= di < n and 0 <= dj < n and union[di][dj] == -1 and l <= abs(matrix[di][dj] - matrix[i][j]) <= r:
+			unity[union_num].append((di, dj))
+			union[di][dj] = union_num
+			dfs(di, dj, union_num)
+
 
 answer = 0
 while True:
+	# dfs 하면서 연합 정보 생성
+	unity = []
 	union = [[-1] * n for _ in range(n)]
-	index = 0
+	union_num = 0
 	for i in range(n):
 		for j in range(n):
 			if union[i][j] == -1:
-				bfs(i, j, index)
-				index += 1
-	if index == n*n:
+				unity.append([(i, j)])
+				union[i][j] = union_num
+				dfs(i, j, union_num)
+				union_num += 1
+
+	# 인구이동 불가? -> break 
+	if union_num == n**2:
 		break
+
+	# 인구이동 연산
+	for uni in unity:
+		total = 0
+		for i, j in uni:
+			total += matrix[i][j]
+		for i, j in uni:
+			matrix[i][j] = total // len(uni)
+			
 	answer += 1
 
 print(answer)
+
 
 file.close()
