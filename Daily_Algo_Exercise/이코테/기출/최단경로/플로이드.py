8bcc1eac89a4ad6502d501b11929d79b25e5e2fd
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 04:25:39 +0000

    [최단경로] 플로이드
    - 무한 경로는 0으로 출력하는 것처럼 문제 좀 봐라 이놈아

--- Daily_Algo_Exercise/이코테/기출/최단경로/플로이드.py
+++ Daily_Algo_Exercise/이코테/기출/최단경로/플로이드.py
@@ -2,30 +2,26 @@ file = open("./Daily_Algo_Exercise/이코테/기출/최단경로/플로이드.tx
 
 input = file.readline 
 
+
 n = int(input())
 m = int(input())
-matrix = [[1e9 for _ in range(n+1)] for _ in range(n+1)]
+matrix = [[1e9] * (n+1) for _ in range(n+1)]
 for _ in range(m):
 	a, b, c = map(int, input().split())
 	matrix[a][b] = min(matrix[a][b], c)
 
-for i in range(1, n + 1):
-	for j in range(1, n + 1):
-		if i == j:
-			matrix[i][i] = 0
-
-for k in range(1, n + 1):
-	for i in range(1, n + 1):
-		for j in range(1, n + 1):
+for k in range(1, n+1):
+	for i in range(1, n+1):
+		for j in range(1, n+1):
+			if i == j:
+				matrix[i][j] = 0
+				continue
 			matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
 
-for i in range(1, n + 1):
-	for j in range(1, n + 1):
-		if matrix[i][j] == 1e9:
-			matrix[i][j] = 0
-		print(matrix[i][j], end=" ")
+for i in range(1, n+1):
+	for j in range(1, n+1):
+		print(matrix[i][j] if matrix[i][j] != 1e9 else 0, end=" ")
 	print()
-
 
 
 file.close()
