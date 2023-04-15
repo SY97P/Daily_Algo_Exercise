2c0c19e5730c5fd44e349b907e796ac3bc3eb913
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 03:40:35 +0000

    [다이나믹] 병사 배치하기
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/다이나믹/병사배치하기.py
+++ Daily_Algo_Exercise/이코테/기출/다이나믹/병사배치하기.py
@@ -1,15 +1,19 @@
 n = int(input())
-array = list(map(int, input().split())) + [0]
+array = list(map(int, input().split()))
+
+# n = 7
+# array = [15, 11, 4, 8, 5, 2, 4]
+
 array.reverse()
+array = [0] + array
 
 dp = [0] * (n+1)
 
 for i in range(1, n+1):
-	max_val = 0
-	for j in range(i-1, -1, -1):
+	max_val = -1
+	for j in range(i):
 		if array[j] < array[i]:
-			max_val = max(max_val, dp[j]+1)
-	dp[i] = max_val
+			max_val = max(max_val, dp[j])
+	dp[i] = max_val + 1
 
-# print(dp)
 print(n - max(dp))
