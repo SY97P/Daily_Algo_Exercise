a7d82cb638df11a29e17dde5ec4b0a42b652ae2f
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 05:23:01 +0000

    [그래프이론] 어두운 길
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/그래프이론/어두운길.py
+++ Daily_Algo_Exercise/이코테/기출/그래프이론/어두운길.py
@@ -2,14 +2,17 @@ file = open("./Daily_Algo_Exercise/이코테/기출/그래프이론/어두운길
 
 input = file.readline 
 
+
 n, m = map(int, input().split())
 edges = []
+total_cost = 0
 for _ in range(m):
 	a, b, c = map(int, input().split())
+	total_cost += c
 	edges.append((c, a, b))
 edges.sort()
 
-parent = [i for i in range(n)]
+parent = [i for i in range(n+1)]
 
 def find(parent, num):
 	if parent[num] != num:
@@ -24,13 +27,12 @@ def union(parent, a, b):
 	elif fa > fb:
 		parent[fa] = fb
 
-answer = 0
-for c, a, b in edges:
-	if find(parent, a) != find(parent, b):
-		union(parent, a, b)
-	else:
-		answer += c
+need_cost = 0
+for c, s, e in edges:
+	if find(parent, s) != find(parent, e):
+		need_cost += c
+		union(parent, s, e)
+print(total_cost - need_cost)
 
-print(answer)
 
 file.close()
