c65e62b1351690662da56f6bd8931742fa9ba38c
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 03:55:57 +0000

    [다이나믹] 못생긴 수
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/다이나믹/편집거리.py
+++ Daily_Algo_Exercise/이코테/기출/다이나믹/편집거리.py
@@ -1,25 +1,17 @@
-a = input().strip()
-b = input().strip()
+a = "cat"
+b = "cut"
+
+a = list(input().strip())
+b = list(input().strip())
 
 n, m = len(a), len(b)
 
-dp = [[0] * (m) for _ in range(n)]
+dp = [[0] * (m+1) for _ in range(n+1)]
 
-if a[0] != b[0]:
-	dp[0][0] = 1
-for i in range(1,m):
-	if a[0] != b[i]:
-		dp[0][i] = dp[0][i-1] + 1
-for i in range(1, n):
-	if a[i] != b[0]:
-		dp[i][0] = dp[i-1][0] + 1
+for i in range(n):
+	dp[i][0] = i
+for i in range(m):
+	dp[0][i] = i
 
-for i in range(1, n):
-	for j in range(1, m):
-		if a[i] == b[j]:
-			dp[i][j] = dp[i-1][j-1]
-		else:
-			dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-
-print(dp[-1][-1])
-		
+for d in dp:
+	print(d)
