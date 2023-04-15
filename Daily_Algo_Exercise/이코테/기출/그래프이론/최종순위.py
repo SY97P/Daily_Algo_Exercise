22b2a6cf457ee478260046bd07afe405393bbf81
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 06:21:32 +0000

    [그래프이론] 최종순위
    - 시벌롬의 오타 그거 아주 빡치네

--- Daily_Algo_Exercise/이코테/기출/그래프이론/최종순위.py
+++ Daily_Algo_Exercise/이코테/기출/그래프이론/최종순위.py
@@ -2,29 +2,31 @@ file = open("./Daily_Algo_Exercise/이코테/기출/그래프이론/최종순위
 
 input = file.readline 
 
+
 from collections import deque
 
 t = int(input())
 for tc in range(t):
 	n = int(input())
-	degree = list(map(int, input().split()))
+	grade = list(map(int, input().split()))
 	indgree = [0] * (n+1)
-	graph = [[False] * (n+1) for _ in range(n+1)]
-	for i in range(n):
+	matrix = [[False] * (n+1) for _ in range(n+1)]
+	for i in range(n-1):
 		for j in range(i+1, n):
-			graph[degree[i]][degree[j]] = True 
-			indgree[degree[j]] += 1
+			matrix[grade[i]][grade[j]] = True
+			indgree[grade[j]] += 1
+			
 	m = int(input())
 	for _ in range(m):
 		a, b = map(int, input().split())
-		if graph[a][b]:
-			graph[a][b] = False
-			graph[b][a] = True
+		if matrix[a][b]:
+			matrix[a][b] = False 
+			matrix[b][a] = True
 			indgree[a] += 1
 			indgree[b] -= 1
 		else:
-			graph[a][b] = True
-			graph[b][a] = False
+			matrix[a][b] = True
+			matrix[b][a] = False
 			indgree[a] -= 1
 			indgree[b] += 1
 
@@ -33,32 +35,33 @@ for tc in range(t):
 		if indgree[i] == 0:
 			q.append(i)
 
-	cycle = False
-	impos = False
 	result = []
-	for _ in range(n):
-		if len(q) > 1:
-			impos = True
-			break
-		elif len(q) == 0:
+	cycle = False
+	incorr = False
+	for k in range(n):
+		if not q:
 			cycle = True
 			break
-		now = q.popleft()
-		result.append(now)
-		for i in range(1, n + 1):
-			if graph[now][i]:
+		if len(q) > 1:
+			incorr = True
+			break
+		node = q.popleft()
+		result.append(node)
+		for i in range(1,n+1):
+			if matrix[node][i]:
 				indgree[i] -= 1
 				if indgree[i] == 0:
 					q.append(i)
 
-
 	if cycle:
-		print("IMPOSSIBLE")
-	elif impos:
+		print("IMPSSIBLE")
+	elif incorr:
 		print("?")
 	else:
-		print(* result)
-		
+		for r in result:
+			print(r, end=" ")
+		print()
+			
 	
 
 file.close()
