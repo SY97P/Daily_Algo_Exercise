86120eb663134e024b10d55cea64213b23460a15
SY97P <54990890+SY97P@users.noreply.github.com>
2023-04-15 04:18:15 +0000

    [다이나믹] 편집 거리
    - 문제 풀이 완료

--- Daily_Algo_Exercise/이코테/기출/다이나믹/편집거리.py
+++ Daily_Algo_Exercise/이코테/기출/다이나믹/편집거리.py
@@ -1,17 +1,25 @@
 a = "cat"
 b = "cut"
 
+a = "sunday"
+b = "saturday"
+
 a = list(input().strip())
 b = list(input().strip())
 
 n, m = len(a), len(b)
 
 dp = [[0] * (m+1) for _ in range(n+1)]
-
-for i in range(n):
+for i in range(1, n+1):
 	dp[i][0] = i
-for i in range(m):
+for i in range(1, m+1):
 	dp[0][i] = i
 
-for d in dp:
-	print(d)
+for i in range(n):
+	for j in range(m):
+		if a[i] == b[j]:
+			dp[i+1][j+1] = dp[i][j]
+		else:
+			dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j], dp[i][j]) + 1
+
+print(dp[-1][-1])
