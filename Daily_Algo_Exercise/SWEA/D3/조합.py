c3d181bb055aedf09fa35c8579772c69305051c7
SY97P <54990890+SY97P@users.noreply.github.com>
2023-05-19 05:14:24 +0000

    [SWEA][D3] 조합
    - 페르마의 소정리

--- Daily_Algo_Exercise/SWEA/D3/조합.py
+++ Daily_Algo_Exercise/SWEA/D3/조합.py
@@ -1,35 +1,29 @@
-#1 45
-
-file = open("./SWEA/D3/조합.txt", "r")
+file = open("./Daily_Algo_Exercise/SWEA/D3/조합.txt")
 
 input = file.readline
 
-import math
+mod = 1234567891
+def pow(num, p):
+	if p == 0:
+		return 1
+	half = pow(num, p//2)
+	if p % 2 == 0:
+		return (half%mod) * (half%mod) % mod
+	else:
+		return ((half*num)%mod) * (half%mod) % mod
 
 t = int(input())
-
-# 2^5 = 2^2 * 2^2 * 2^1
-# 2^4 = 2^2 * 2^2
-def pow(a, b) : 
-	global p 
-	
-	if b == 0 : 
-		return 1
-	sub = pow(a, b//2)
-	result = (sub ** 2) % p
-
-	if b % 2 == 0 : 
-		return result
-	else : 
-		return (result * a) % p
-
-for tc in range(1, t + 1) : 
+for tc in range(1, t+1):
 	n, r = map(int, input().split())
+	dp = [1] * (n+1)
+	for i in range(2, n+1):
+		dp[i] = (dp[i-1] * i) % mod
 
-	p = 1234567891
+	top = dp[n]%mod
+	bottom = ((dp[r]%mod) * (dp[n-r]%mod))%mod
 
-	result = math.factorial(n) * pow(math.factorial(r) * math.factorial(n-r) % p , p - 2) % p
+	perma_top = pow(bottom, mod-2)
 
-	print("#%d %d" %(tc, result))
+	print(f'#{tc} {int(top * perma_top)%mod}')
 
 file.close()
